e4ee482ffc69ea9188e833f2fcdd0c7b8c1679fb
Carlo Beltrame <carlo@beltra.me>
2022-03-29 16:29:16 +0200

    CTA on the process list should link to labcity.ch

--- app/cells/decidim_biel/participatory_processes/process_m_cell.rb
+++ app/cells/decidim_biel/participatory_processes/process_m_cell.rb
@@ -11,6 +11,10 @@ module DecidimBiel
         def resource_image_path
           model.hero_image.url || current_organization.highlighted_content_banner_image.url
         end
+
+        def step_cta_path
+          'https://www.labcity.ch'
+        end
       end
     end
   end
