bb709ce7f04861fbc494cb0778d3bad061f0ba60
Carlo Beltrame <carlo@beltra.me>
2022-04-06 16:11:34 +0200

    Revert forced change of call-to-action

--- app/cells/decidim_biel/participatory_processes/process_m_cell.rb
+++ app/cells/decidim_biel/participatory_processes/process_m_cell.rb
@@ -11,10 +11,6 @@ module DecidimBiel
         def resource_image_path
           model.hero_image.url || current_organization.highlighted_content_banner_image.url
         end
-
-        def step_cta_path
-          'https://www.labcity.ch'
-        end
       end
     end
   end
