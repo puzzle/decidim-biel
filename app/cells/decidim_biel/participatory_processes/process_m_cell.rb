85dcada4693bc51d5c69a80490aa4acb359d49ca
Thomas Burkhalter <burkhalter@puzzle.ch>
2022-07-08 16:42:52 +0200

    Fix default images

--- app/cells/decidim_biel/participatory_processes/process_m_cell.rb
+++ app/cells/decidim_biel/participatory_processes/process_m_cell.rb
@@ -9,7 +9,14 @@ module DecidimBiel
 
       included do
         def resource_image_path
-          model.hero_image.url || current_organization.highlighted_content_banner_image.url
+          # model.hero_image.url || current_organization.highlighted_content_banner_image.url
+          if model.hero_image.attached?
+            model.attached_uploader(:hero_image).path
+          elsif current_organization.highlighted_content_banner_image.url.present?
+            current_organization.highlighted_content_banner_image.url
+          else
+            asset_pack_path('media/images/organization-default-image.png')
+          end
         end
       end
     end
