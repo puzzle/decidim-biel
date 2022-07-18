a1717a572434a8a59735f9d18dabe71fdd16e782
Thomas Burkhalter <burkhalter@puzzle.ch>
2022-07-18 17:33:24 +0200

    Fix process_m_cell display

--- app/cells/decidim_biel/participatory_processes/process_m_cell.rb
+++ app/cells/decidim_biel/participatory_processes/process_m_cell.rb
@@ -12,8 +12,8 @@ module DecidimBiel
           # model.hero_image.url || current_organization.highlighted_content_banner_image.url
           if model.hero_image.attached?
             model.attached_uploader(:hero_image).path
-          elsif current_organization.highlighted_content_banner_image.url.present?
-            current_organization.highlighted_content_banner_image.url
+          elsif current_organization.highlighted_content_banner_image.present?
+            rails_blob_path(current_organization.highlighted_content_banner_image)
           else
             asset_pack_path('media/images/organization-default-image.png')
           end
